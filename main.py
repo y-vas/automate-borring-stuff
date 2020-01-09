17a51a7c6f2575cdf75c0e0c33b707f002c6ca97
Vasyl Yovdiy <yovdiyvasyl@gmail.com>
2020-01-09 04:23:28 +0100

    follow a lot of people

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 from selenium import webdriver
 from time import sleep
-from secrets import pw
+from secrets import *
 
 
 class InstaBot:
     def __init__(self, username, pw):
         self.driver = webdriver.Chrome()
-        self.username = username
+        self.username = un
         self.driver.get("https://instagram.com")
+
         sleep(2)
-        self.driver.find_element_by_xpath("//a[contains(text(), 'Log in')]")\
+        self.driver.find_element_by_xpath("//a[contains(text(), 'Entrar')]")\
             .click()
         sleep(2)
         self.driver.find_element_by_xpath("//input[@name=\"username\"]")\
-            .send_keys(username)
+            .send_keys(un)
         self.driver.find_element_by_xpath("//input[@name=\"password\"]")\
             .send_keys(pw)
         self.driver.find_element_by_xpath('//button[@type="submit"]')\
             .click()
         sleep(4)
-        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Ahora no')]")\
             .click()
         sleep(2)
 
@@ -41,22 +42,34 @@ class InstaBot:
         sugs = self.driver.find_element_by_xpath('//h4[contains(text(), Suggestions)]')
         self.driver.execute_script('arguments[0].scrollIntoView()', sugs)
         sleep(2)
-        scroll_box = self.driver.find_element_by_xpath("/html/body/div[3]/div/div[2]")
+        scroll_box = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[2]')
         last_ht, ht = 0, 1
+
         while last_ht != ht:
             last_ht = ht
-            sleep(1)
+
+            sleep( 1 )
             ht = self.driver.execute_script("""
-                arguments[0].scrollTo(0, arguments[0].scrollHeight); 
+                arguments[0].scrollTo(0, arguments[0].scrollHeight);
                 return arguments[0].scrollHeight;
-                """, scroll_box)
+                """, scroll_box
+            )
+
         links = scroll_box.find_elements_by_tag_name('a')
-        names = [name.text for name in links if name.text != '']
-        # close button
-        self.driver.find_element_by_xpath("/html/body/div[3]/div/div[1]/div/div[2]/button")\
-            .click()
+        names = [ name.text for name in links if name.text != '' ]
+
+        foll = scroll_box.find_elements_by_xpath('//button[contains(text(), Seguir)]')
+
+        for fb in foll:
+            sleep(1)
+            fb.click()
+
         return names
 
+    def _followall(self):
+        self.driver.get("https://www.instagram.com/explore/people/suggested/")
+        print(self._get_names())
 
-my_bot = InstaBot('_aaronjack', pw)
-my_bot.get_unfollowers()
+
+my_bot = InstaBot(un, pw)
+my_bot._followall()
